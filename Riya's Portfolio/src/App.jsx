ca01696c59d84cf44e3fe946b7316d5595cc0466
riya2509 <91462077+riya2509@users.noreply.github.com>
2023-12-16 13:30:35 +0530

    Adding home route

--- Riya's Portfolio/src/App.jsx
+++ Riya's Portfolio/src/App.jsx
@@ -1,10 +1,28 @@
 // import React from "react";
 import NavBar from "./Components/Navbar";
+import Home from "./Components/Home/Home";
+// import Background from "./Components/Home/Background";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  // Navigate
+} from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
     <div>
-      <NavBar />
+      {/* <Background /> */}
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* <Route path="/project" element={<Projects />} />
+        <Route path="/about" element={<About />} />
+      <Route path="/resume" element={<Resume />} /> */}
+        </Routes>
+      </Router>
     </div>
   );
 }
