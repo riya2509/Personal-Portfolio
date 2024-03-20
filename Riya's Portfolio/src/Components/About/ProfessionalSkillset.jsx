b94c937f6f9e49585bd346a8eb541b1ce4b6a9f5
riya2509 <91462077+riya2509@users.noreply.github.com>
2024-03-20 09:32:43 +0530

    Professional Skillset

--- Riya's Portfolio/src/Components/About/ProfessionalSkillset.jsx
+++ Riya's Portfolio/src/Components/About/ProfessionalSkillset.jsx
@@ -1,5 +1,17 @@
-import { Container, Row } from "react-bootstrap";
-
+import { Container, Row, Col } from "react-bootstrap";
+import {
+  DiJavascript1,
+  DiReact,
+  DiNodejs,
+  DiPython,
+  DiGit,
+  DiJava,
+} from "react-icons/di";
+import { SiMysql, SiExpress, SiMui } from "react-icons/si";
+import { IoLogoCss3 } from "react-icons/io5";
+import { BsBootstrapFill } from "react-icons/bs";
+import { TiHtml5 } from "react-icons/ti";
+import "./ProfessionalSkillset.css";
 function ProfessionalSkillset() {
   return (
     <div>
@@ -17,6 +29,44 @@ function ProfessionalSkillset() {
         </Row>
         <Row></Row>
       </Container>
+      <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+        <Col xs={4} md={2} className="tech-icons">
+          <DiJavascript1 />
+        </Col>
+        <Col xs={4} md={2} className="tech-icons">
+          <DiNodejs />
+        </Col>
+        <Col xs={4} md={2} className="tech-icons">
+          <DiReact />
+        </Col>
+        <Col xs={4} md={2} className="tech-icons">
+          <DiGit />
+        </Col>
+        <Col xs={4} md={2} className="tech-icons">
+          <DiPython />
+        </Col>
+        <Col xs={4} md={2} className="tech-icons">
+          <DiJava />
+        </Col>
+        <Col xs={4} md={2} className="tech-icons">
+          <SiMysql />
+        </Col>
+        <Col xs={4} md={2} className="tech-icons">
+          <SiExpress />
+        </Col>
+        <Col xs={4} md={2} className="tech-icons">
+          <TiHtml5 />
+        </Col>
+        <Col xs={4} md={2} className="tech-icons">
+          <IoLogoCss3 />
+        </Col>
+        <Col xs={4} md={2} className="tech-icons">
+          <SiMui />
+        </Col>
+        <Col xs={4} md={2} className="tech-icons">
+          <BsBootstrapFill />
+        </Col>
+      </Row>
     </div>
     // Adding skill set
   );
